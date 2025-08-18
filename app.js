368aee25d7b19ee058c7f1b89fde2ec50195b2ea
Eghosa Elaiho <eghosaelaiho@gmail.com>
2025-08-18 11:26:27 -0600

    update

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
 
 
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
+
+// api endpoint exposing user resourse
+app.get('/api/vi/user', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}); 
 
 mongoose.connect(uri).then(
 async ()=> {
