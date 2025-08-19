5fc99cd5acd922c7560a8ae8ae7fc1d64101f3e2
Eghosa Elaiho <eghosaelaiho@gmail.com>
2025-08-19 10:48:20 -0600

    update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const ejs = require('ejs')
 const PORT = process.env.PORT;
 const uri = process.env.MONGO_URI;
-
+const { readData } = require('./utils/file.js');
 
 
 app.set('views', './views');
@@ -13,24 +13,24 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-// api endpoint exposing user resourse
-app.get('/api/vi/user', async (req, res) => {
+//api endpoint exposing user resources
+app.get('/api/v1/users',  async (req, res) => {
     try {
-        const users = await User.find();
-        res.json(users);
+        const data = await readData();
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}); 
+});
+
 
 mongoose.connect(uri).then(
 async ()=> {
-    console.log("Connected to MongoDB successfully");
 
-    
+    console.log('Connected to MongoDB Server');
+
     app.listen(PORT, () => {
         console.log(`Connected to port ${PORT}`)
     });
